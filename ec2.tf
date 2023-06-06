aa0d4feb6da2ce5b34e1c3f05d8a1059f1824585
Dushyant737 <55356449+Dushyant737@users.noreply.github.com>
2023-06-06 17:55:39 +0530

    Update ec2.tf
    updated SG details

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "dkec3" {
     key_name = "iam_dushyant_vsf_ec2_key_pairs"
     subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.dksg_1.id]
+    vpc_security_group_ids = [aws_security_group.dksg_2.id]
   
 }
